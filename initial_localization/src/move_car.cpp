6d2153f982ccc154e26772c40bc641203c67d763
Peter <peter.leidl@gmx.de>
2016-01-12 19:27:58 +0100

    Initial Localization, velocity control redesigned

--- initial_localization/src/move_car.cpp
+++ initial_localization/src/move_car.cpp
@@ -1,60 +1,85 @@
+/*
+ * Node to move car for Monte Carlo Localization
+ * 
+ * authors: Peter Leidl, Dominik Mücke
+ * */
+
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "sensor_msgs/LaserScan.h"
 
+//Callback function which publishes commands to move
+void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg){
+
+ ros::NodeHandle nh;
+ ros::Publisher pub_movement = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
+ 
+ //Display distance to obstacle in front of the car
+ ROS_INFO("%f", scan_msg->ranges[300]);
+ 
+ //Check if distance is big enough to move car forward
+ if(scan_msg->ranges[300] >= 1){
+  geometry_msgs::Twist move_cmd;
+  geometry_msgs::Twist move_cmd1;
+  geometry_msgs::Twist move_cmd2;
+  geometry_msgs::Twist move_cmd3;
+
+  ros::Rate loop_rate(0.10);
+  
+  //Velocity command
+  move_cmd.linear.x = 1.8;
+  move_cmd.linear.y = 0;
+  move_cmd.linear.z = 0;
+  
+  //Correction of angular velocity
+  move_cmd.angular.x = 0;
+  move_cmd.angular.y = 0;
+  move_cmd.angular.z = 0.31;
+ 
+  pub_movement.publish(move_cmd);
+ 
+  loop_rate.sleep();
+ 
+  //Velocity command
+  move_cmd1.linear.x = -1;
+  move_cmd1.linear.y = 0;
+  move_cmd1.linear.z = 0;
+
+  //Correction of angular velocity
+  move_cmd1.angular.x = 0;
+  move_cmd1.angular.y = 0;
+  move_cmd1.angular.z = -0.2;
+ 
+  pub_movement.publish(move_cmd1);
+ 
+  loop_rate.sleep();
+ 
+  //Stop moving car
+  move_cmd3.linear.x = 0;
+  move_cmd3.linear.y = 0;
+  move_cmd3.linear.z = 0;
+  
+  move_cmd3.angular.x = 0;
+  move_cmd3.angular.y = 0;
+  move_cmd3.angular.z = 0;
+ 
+ pub_movement.publish(move_cmd3);
+
+ ros::shutdown(); 
+ }
+
+}
+
+//Main function initialzes node
 int main(int argc, char **argv){
   
   ros::init(argc, argv, "move_car");
   ros::NodeHandle n;
-  ros::Rate loop_rate(0.1);
-  
-  ros::Publisher pub_movement = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
-  ROS_INFO("Drinnen");
-  
-  geometry_msgs::Twist move_cmd;
-  geometry_msgs::Twist move_cmd1;
-  geometry_msgs::Twist move_cmd2;
-  
-  move_cmd.linear.x = 1;
-  move_cmd.linear.y = 0;
-  move_cmd.linear.z = 0;
-  
-  move_cmd.angular.x = 0;
-  move_cmd.angular.y = 0;
-  move_cmd.angular.z = 0;
- 
- pub_movement.publish(move_cmd);
- ROS_INFO("Vorwärts");
- 
- loop_rate.sleep();
- 
-  move_cmd1.linear.x = -1;
-  move_cmd1.linear.y = 0;
-  move_cmd1.linear.z = 0;
-  
-  move_cmd1.angular.x = 0;
-  move_cmd1.angular.y = 0;
-  move_cmd1.angular.z = 0;
- 
- pub_movement.publish(move_cmd1);
- ROS_INFO("Rück");
- loop_rate.sleep();
- 
-  move_cmd2.linear.x = 1;
-  move_cmd2.linear.y = 0;
-  move_cmd2.linear.z = 0;
-  
-  move_cmd2.angular.x = 0;
-  move_cmd2.angular.y = 0;
-  move_cmd2.angular.z = 0;
- 
- pub_movement.publish(move_cmd2);
- ROS_INFO("Vorwärts");
- ROS_INFO("Ende");
 
- 
- 
- 
- ros::spinOnce();
+  ros::Subscriber sub_scan = n.subscribe<sensor_msgs::LaserScan> ("/scan", 1, scanCallback);
+  
+
+ ros::spin();
     
  return 0;
  
--- tas_autonomous_control/src/tas_autonomous_control_node.cpp
+++ tas_autonomous_control/src/tas_autonomous_control_node.cpp
@@ -1,40 +1,111 @@
+/*
+File to control velocity depending on distance between the front of the car to obstacles.
+Increase velocity if there much space and no obstacle in front.
+Decrease velocity if there is an obstacle near the front of the car or we reach a curve.
+
+authors: Peter Leidl, Dominik Mücke
+*/
+
 #include "control/control.h"
+#include "sensor_msgs/LaserScan.h"
+
+int check_dist;
+
+/*Callback function
+  Checks the scan which is in front of the car
+  Assigns check_dist flag corresponding to distance
+*/
+void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg){
+
+    if(scan_msg->ranges[300] > 2.0){
+        check_dist = 0;
+    }
+
+    if(scan_msg->ranges[300] > 1.0 && scan_msg->ranges[300] < 2.0){
+        check_dist = 1;
+    }
+
+    if(scan_msg->ranges[300] > 0.2 && scan_msg->ranges[300] < 1.0){
+        check_dist = 2;
+    }
+    if(scan_msg->ranges[300] < 0.2){
+        check_dist = 3;
+    }
+}
+
+/*
+Main function initialize node and subscribe Laser Scan
+Based on the subscribed Laser Scan we choose the max velocity
+*/
 
 int main(int argc, char** argv)
 {
+    //Initialze node and Subscriber
     ros::init(argc, argv, "autonomous_control");
     control autonomous_control;
-
+    ros::NodeHandle n;
+    ros::Subscriber sub_scan = n.subscribe<sensor_msgs::LaserScan> ("/scan", 1, scanCallback);
     ros::Rate loop_rate(50);
 
+    //Loop to assign velocity
     while(ros::ok())
     {
+	//Check if car is navigated with remote control
         if(autonomous_control.control_Mode.data==0)
         {
             ROS_INFO("Manually Control!");
         }
+
         else
         {
+	  //Autonomous button on remote control is pressed
+	  ROS_INFO("Automatic Control!");
+	    
+            //control_servo == 1500 --> car is not moving
             if(autonomous_control.control_Brake.data==1)
             {
                 autonomous_control.control_servo.x=1500;
                 autonomous_control.control_servo.y=1500;
             }
+		
+	    
             else
             {
-                ROS_INFO("Automatic Control!");
-                if(autonomous_control.cmd_linearVelocity>0)
+                ROS_INFO("%f", autonomous_control.cmd_linearVelocity);
+                if(check_dist == 0)
                 {
+                    ROS_INFO("Distance more than 2 meters in front of the car!");
                     autonomous_control.control_servo.x = 1550;
                 }
-                else if(autonomous_control.cmd_linearVelocity<0)
+                else if(check_dist == 1)
                 {
-                    autonomous_control.control_servo.x = 1300;
+                    ROS_INFO("Distance between 1 and 2 meters in front of the car!");
+                    autonomous_control.control_servo.x = 1550;
                 }
-                else
+                else if(check_dist == 2)
                 {
+                    ROS_INFO("Dsiatnce between 0.2 and 1 meters in front of the car!");
+                    autonomous_control.control_servo.x = 1530;
+                }
+                else if(check_dist == 3)
+                {
+            	    ROS_INFO("STOP CAR!");
                     autonomous_control.control_servo.x = 1500;
                 }
+
+                if(autonomous_control.cmd_linearVelocity < 0)
+		{
+		    ROS_INFO("Driving backwards");
+                    autonomous_control.control_servo.x = 1300;
+                }
+
+                else if(autonomous_control.cmd_linearVelocity == 0)
+                {
+            	    ROS_INFO("Car not moving!");
+                    autonomous_control.control_servo.x = 1500;
+                }
+
+
 
                 autonomous_control.control_servo.y = autonomous_control.cmd_steeringAngle;
             }
